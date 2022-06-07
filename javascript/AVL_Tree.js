cdf03d5e741b620193fe692ac96b698ad01cf964
Artyom <minicoal@gmail.com>
2022-06-07 14:06:56 +1000

    Added AVL code and printing

---
+++ javascript/AVL_Tree.js
@@ -0,0 +1,202 @@
+class Node {
+    constructor(key) {
+        this.key = key;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
+
+        this.x = 0;
+        this.y = 0;
+        this.parent = null;
+    }
+}
+
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    height(node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    rightRotate(x) {
+        let y = x.left;
+        let T2 = y.right;
+
+        y.right = x;
+        x.left = T2;
+
+        x.height = Math.max(this.height(x.left), this.height(x.right)) + 1;
+        y.height = Math.max(this.height(y.left), this.height(y.right)) + 1;
+
+        return y;
+    }
+
+    leftRotate(x) {
+        let y = x.right;
+        let T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = Math.max(this.height(x.left), this.height(x.right)) + 1;
+        y.height = Math.max(this.height(y.left), this.height(y.right)) + 1;
+
+        return y;
+    }
+
+    getBalance(node) {
+        if (node == null) 
+            return 0;
+        return this.height(node.left) - this.height(node.right);
+    }
+
+    insert(node, key) {
+        if (node == null) 
+            return new Node(key);
+
+        if (key < node.key) 
+            node.left = this.insert(node.left, key);
+
+        else if (key > node.key) 
+            node.right = this.insert(node.right, key);
+
+        else
+            return node;
+
+        node.height = Math.max(this.height(node.left), this.height(node.right)) + 1;
+
+        let balance = this.getBalance(node);
+        if (balance > 1 && key < node.left.key)
+            return this.rightRotate(node);
+ 
+        if (balance < -1 && key > node.right.key)
+            return this.leftRotate(node);
+ 
+        if (balance > 1 && key > node.left.key) {
+            node.left = this.leftRotate(node.left);
+            return this.rightRotate(node);
+        }
+
+        if (balance < -1 && key < node.right.key) {
+            node.right = this.rightRotate(node.right);
+            return this.leftRotate(node);
+        }
+
+        return node;
+    }
+
+    Insert(key) {
+        this.root = this.insert(this.root, key);
+    }
+
+    minValueNode(node) {
+        let temp = node;
+        while (temp.left != null) {
+            temp = temp.left;
+        }
+        return temp;
+    }
+
+    remove(node, key) {
+        if (node == null) 
+            return null;
+
+        if (key < node.key) 
+            node.left = this.remove(node.left, key);
+
+        else if (key > node.key) 
+            node.right = this.remove(node.right, key);
+
+        else
+        {
+            if (node.left == null || node.right == null) {
+                let temp = null;
+
+                if (node.left != null) 
+                    temp = node.left;
+                else if (node.right != null) {
+                    temp = node.right;
+                }
+
+                if (temp == null) {
+                    temp = node;
+                    node = null;
+                }
+                else
+                    node = temp;
+            }
+            else {
+                let temp = this.minValueNode(node.right);
+                node.key = temp.key;
+                node.right = this.remove(node.right, temp.key)
+            }
+        }
+        if (node == null) 
+            return null;
+
+        node.height = Math.max(this.height(node.left), this.height(node.right)) + 1;
+
+        let balance = this.getBalance(node);
+
+        if (balance > 1 && this.getBalance(node.left) >= 0)
+            return this.rightRotate(node);
+        if (balance < -1 && this.getBalance(node.right) <= 0)
+            return this.leftRotate(node);
+        if (balance > 1 && this.getBalance(node.left) < 0)
+        {
+            node.left = this.leftRotate(node.left);
+            return this.rightRotate(node);
+        }
+        if (balance < -1 && this.getBalance(node.right) > 0)
+        {
+            node.right = this.rightRotate(node.right);
+            return this.leftRotate(node);
+        }
+   
+        return node;
+    }
+
+    Remove(key) {
+        this.root = this.remove(this.root, key);
+    }
+
+    levelPrint(node, level, currLevel) {
+        if (node == null)
+            return;
+        if (currLevel == 1) {
+            const nodeOffset = 1 / (Math.abs(node.y) + 1) * level;
+            if (node.left != null) {
+                node.left.x = node.x - nodeOffset;
+                node.left.y = node.y - 1;
+
+                node.left.parent = node;
+            }
+            if (node.right != null) {
+                node.right.x = node.x + nodeOffset;
+                node.right.y = node.y - 1;
+
+                node.right.parent = node;
+            }
+        }
+        else if (currLevel > 1){
+            this.levelPrint(node.left, level, currLevel - 1);
+            this.levelPrint(node.right, level, currLevel - 1);
+        }
+    }
+
+    levelOrderPrint(node) {
+        for (let i = 1; i < node.height; ++i) {
+            this.levelPrint(node, node.height - i, i);
+        }
+    }
+    
+    prepareForPrint() {
+        this.levelOrderPrint(this.root);
+    }
+}
+
+
--- javascript/handle.js
+++ javascript/handle.js
@@ -6,7 +6,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
 const fov = 50;
-const aspect = 2;
+const aspect = canvas.offsetWidth / canvas.offsetHeight;
 const near = 0.1;
 const far = 100;
 
@@ -14,6 +14,8 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 5;
 
 const onWindowResize = () => {
+    canvas.style.width = "100%";
+
     camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
     camera.updateProjectionMatrix();
 
@@ -54,7 +56,6 @@ const createCanvas = (text) => {
 
     const resizeCoef = 1.5;
     const textSize = Math.min(canvas.width / text.length, 83);
-    console.log(textSize);
 
     let context = canvas.getContext('2d');
     {
@@ -70,7 +71,6 @@ const createCanvas = (text) => {
         context.fillText(text, canvasWidth / 2, canvasHeight / 2 + 5);
     }
 
-    document.body.appendChild(canvas);
     return canvas;
 }
 
@@ -82,11 +82,11 @@ const addSolidGeometry = (x, y, z, geometry, text) => {
     //creating materials
     const basicColor = nodeColor.hex;
     const materials = [
+        new THREE.MeshBasicMaterial({color: basicColor}),
+        new THREE.MeshBasicMaterial({color: basicColor}),
+        new THREE.MeshBasicMaterial({color: basicColor}),
+        new THREE.MeshBasicMaterial({color: basicColor}),
         new THREE.MeshBasicMaterial({map: texture}),
-        new THREE.MeshBasicMaterial({color: basicColor}),
-        new THREE.MeshBasicMaterial({color: basicColor}),
-        new THREE.MeshBasicMaterial({color: basicColor}),
-        new THREE.MeshBasicMaterial({color: basicColor}),
         new THREE.MeshBasicMaterial({color: basicColor}),
     ];
 
@@ -95,14 +95,58 @@ const addSolidGeometry = (x, y, z, geometry, text) => {
 }
 
 const addNode = (x, y, text = "Sample Text") => {
-    const width = 1;
-    const height = 1;
-    const depth = 1;
+    const width = 0.5;
+    const height = 0.5;
+    const depth = 0.5;
 
     addSolidGeometry(x, y, 0, new THREE.BoxGeometry(width, height, depth), text); 
 }
 
-addNode(0, 0);
+const addLine = (x1, y1, z1, x2, y2, z2) => {
+    const color = 0xFFFFFF;
+
+    const material = new THREE.LineBasicMaterial({
+        color: color,
+    });
+
+    const points = [];
+    points.push(new THREE.Vector3(x1, y1, z1));
+    points.push(new THREE.Vector3(x2, y2, z2));
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+
+    scene.add(line);
+}
+
+const print_ = (root) => {
+    if (root != null) {
+        print_(root.left);
+        addNode(root.x, root.y, root.key.toString());
+        if (root.parent != null) {
+            addLine(root.x, root.y, 0, root.parent.x, root.parent.y, 0);
+        }
+        print_(root.right);
+    }
+}
+
+
+let A = new AVLTree();
+A.Insert(5);
+A.Insert(10);
+A.Insert(0);
+A.Insert(11);
+A.Insert(7);
+A.Insert(12);
+A.Insert(87);
+A.Insert(99);
+A.Insert(100);
+A.Insert(56);
+A.Insert(34);
+
+A.prepareForPrint();
+print_(A.root);
+
 
 function animate(time) {
 	time *= 0.001;
